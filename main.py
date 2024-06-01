4703c03e352c133b3d01ce5c68448db3135cf350
Andrew <andrew.fedosseyenko@gmail.com>
2024-06-01 15:25:04 +0500

    Added bunch of functions and tested TODOs, there's threading for video title check, separated functions file for later additions, the progress bar testing and etc.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import customtkinter # UI libraries
-from tkinter.filedialog import askdirectory
 
 import pytube # YouTube Downloader
 
 from threading import Thread
 
 import os
+from tkinter.filedialog import askdirectory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 icon_path = os.path.join(BASE_DIR, 'resources/icon.ico')
-
+font_path = os.path.join(BASE_DIR, 'resources/font.ttf')
+save_path = BASE_DIR
+# Default path settings
+def set_default_path():
+    global save_path
+    save_path = askdirectory(title='Select a place to save', initialdir=BASE_DIR)
 
 # Main Window settings
 Fast_Cut = customtkinter.CTk()
@@ -17,19 +22,32 @@ Fast_Cut.title('FastCut')
 rootHeight = 600
 rootWidth = 1200
 
+
 # Theme settings
 customtkinter.set_appearance_mode('dark') 
 Fast_Cut.iconbitmap(icon_path)
+normal_font = customtkinter.CTkFont(family=font_path, size=16, weight='normal')
+bold_font = customtkinter.CTkFont(family=font_path, size=16, weight='bold')
 
-#def progress_bar_function(stream, chunk, bytes_remaining):
-#    download_bar.set(float(stream.filesize - bytes_remaining) / stream.filesize * 100)
-#    download_bar.update()
 
-save_path = BASE_DIR
-def set_default_path():
-    global save_path
-    save_path = askdirectory(title='Select a place to save', initialdir=BASE_DIR)
+# Link checking function for call a video title
+def link_check():
+    link = link_entry.get()
+    if link != "":
+        try:
+            yt = pytube.YouTube(link, on_progress_callback = None)
+            video_title = yt.title
+            video_title_label.configure(text = video_title)
+        except pytube.exceptions.RegexMatchError:
+            video_title_label.configure(text = "Wrog link")
+# Threading for link checking
+def link_check_thread(event):
+    thread = Thread(target = link_check, daemon = True)
+    thread.start()
+    return thread
 
+
+# Download function
 def download(save_path):
         link = link_entry.get()
         link_entry.delete(0, 'end')
@@ -41,7 +59,7 @@ def download(save_path):
         stream = yt.streams.first()
         stream.download(save_path)
         print('Downloaded')
-
+# Download button callback
 def download_button():
     global save_path
     if save_path != "":
@@ -52,13 +70,11 @@ def download_button():
             print('Something went wrong')
     else:
         print('Path is None')
-
 # Threading for downloading
 def download_thread(save_path):
     thread = Thread(target = download, args=(save_path,), daemon = True)
     thread.start()
     return thread
-
 
 
 # Background frame settings
@@ -67,18 +83,24 @@ background.pack(fill='both', expand=True)
 
 # Download bar settings
 download_bar = customtkinter.CTkProgressBar(master=background, width=500, height=20, fg_color="#131324", progress_color='#6558FF')
-download_bar.place(anchor = 'center', relx = 0.5, rely = 0.42)
+download_bar.place(anchor = 'center', relx = 0.5, rely = 0.70)
+
+# Video Title settings
+video_title_label = customtkinter.CTkLabel(master=background, text='VIDEO TITLE', bg_color="#131324", fg_color="#131324", text_color="#6558FF", font=bold_font, width=500, height=30)
+video_title_label.place(anchor = 'center', relx = 0.5, rely = 0.78)
 
 # Link entry settings
 link_entry = customtkinter.CTkEntry(master=background, width=500, height=30, placeholder_text="Enter link", fg_color='#131324',)
-link_entry.place(anchor = 'center', relx = 0.5, rely = 0.5)
+link_entry.place(anchor = 'center', relx = 0.5, rely = 0.83)
+link_entry.bind("<KeyRelease>", link_check_thread)
 
+# Path button settings
 path_button = customtkinter.CTkButton(master=background, width=50, text="Path", fg_color="#131324", command=set_default_path)
-path_button.place(anchor= 'center', relx = 0.687, rely = 0.6)
+path_button.place(anchor= 'center', relx = 0.687, rely = 0.9)
 
 # Download button settings
 button = customtkinter.CTkButton(master=background, text="Download", fg_color="#131324", command=download_button)
-button.place(anchor= 'center', relx = 0.35, rely = 0.6)
+button.place(anchor= 'center', relx = 0.35, rely = 0.9)
 
 # The window and grid settings
 Fast_Cut.geometry(f'{rootWidth}x{rootHeight}')
@@ -92,3 +114,9 @@ background.grid_columnconfigure(3, weight=1)
 
 # Mainloop start
 Fast_Cut.mainloop()
+
+
+# TODO: Add progress bar
+# TODO: Progress Bar need to appear on the link entry, and disable after finishing download
+
+# TODO: Add frame for video, that will apear from the bottom of the window
--- test.py
+++ test.py
@@ -1,78 +1,22 @@
-import customtkinter # UI library
-import tkinter
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-icon_path = os.path.join(BASE_DIR, 'resources/icon.ico')
-
-# Main Window settings
-Fast_Cut = customtkinter.CTk()
-Fast_Cut.title('FastCut')
-rootHeight = 500
-rootWidth = 1000
-
-# Theme settings
-customtkinter.set_appearance_mode('dark') 
-Fast_Cut.iconbitmap(icon_path)
+from pytube import YouTube
 
 
-# Background frame settings
-background = customtkinter.CTkFrame(master=Fast_Cut, width=rootWidth, height=rootHeight, fg_color="black")
-background.pack(fill='both', expand=True)
+def on_progress(stream, chunk, bytes_remaining):
+    """Callback function"""
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    pct_completed = bytes_downloaded / total_size * 100
+    print(f"Status: {round(pct_completed, 2)} %")
 
 
-# Side_Panel cless
-class SidePanel(customtkinter.CTkFrame):
-	def __init__(self, master, start_position, end_position):
-		super().__init__(master = master)
+url = "https://youtube.com/watch?v=9bZkp7q19f0"
+yt = YouTube(url, on_progress_callback=on_progress)
 
-		# Launch settings 
-		self.start_position = start_position
-		self.end_position = end_position
-		self.width = abs(start_position - end_position)
+out = yt.streams\
+    .filter(progressive=True, file_extension='mp4')\
+    .order_by('resolution')\
+    .desc()\
+    .first()\
+    .download()
 
-        # Animation
-		self.pos = self.start_position
-		self.in_start_position = True
-
-		# layout
-		self.place(relx = self.start_position, rely = 0.05, relwidth = self.width, relheight = 0.9)
-
-	def animate(self):
-		if self.in_start_position:
-			self.animate_forward()
-		else:
-			self.animate_backwards()
-
-	def animate_forward(self):
-		if self.pos > self.end_position:
-			self.pos -= 0.008
-			self.place(relx = self.pos, rely = 0.05, relwidth = self.width, relheight = 0.9)
-			self.after(10, self.animate_forward)
-		else:
-			self.in_start_pos = False
-
-	def animate_backwards(self):
-		if self.pos < self.start_pos:
-			self.pos += 0.008
-			self.place(relx = self.pos, rely = 0.05, relwidth = self.width, relheight = 0.9)
-			self.after(10, self.animate_backwards)
-		else:
-			self.in_start_pos = True
-side_panel = SidePanel(background, 1.0, 0.2)
-
-button = customtkinter.CTkButton(master=background, text="button", fg_color="#131324", command=side_panel.animate)
-button.pack(pady=20)
-
-# The window and grid settings
-Fast_Cut.geometry(f'{rootWidth}x{rootHeight}')
-Fast_Cut.attributes('-alpha', 1.0)
-
-background.grid_rowconfigure(0, weight=1)
-background.grid_columnconfigure(0, weight=1)
-background.grid_columnconfigure(1, weight=1)
-background.grid_columnconfigure(2, weight=1)
-background.grid_columnconfigure(3, weight=1)
-
-# Mainloop start
-Fast_Cut.mainloop()
+print(f"Download complete: {out}")
